5fc2e9766b9b6b1101beaeb65565200b6c4ece23
Viktor Riga <rigavv@gmail.com>
2020-06-02 07:40:00 +0300

    1

---
+++ HWls7_RigaViktor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWls7_RigaViktor
+//
+//  Created by Viktor Riga on 02.06.2020.
+//  Copyright © 2020 Viktor Riga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
